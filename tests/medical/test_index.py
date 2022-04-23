591bc06ffd227e39759bb6d5b0f4a3e3e77d939d
Enrique Rodriguez <enrique.rodriguez9@upr.edu>
2022-04-23 19:16:10 -0400

    Adds testcase for index page.

---
+++ tests/medical/test_index.py
@@ -0,0 +1,19 @@
+from medical.urls import index
+from django.test import TestCase
+from django.urls import resolve, reverse
+
+
+class TestIndexPage(TestCase):
+
+    def setUp(self):
+        self.url = reverse("index")
+
+    def test_url_resolves(self):
+        resolution = resolve(self.url).func
+
+        self.assertEqual(resolution, index)
+    
+    def test_GET(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
