e97bf4031f2b0d156d8f7d5eddff026f42e33cd5
Enrique Rodriguez <enrique.rodriguez9@upr.edu>
2022-04-25 19:38:24 -0400

    Adds install frontend

--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-REQUIRED="git pipenv"
+REQUIRED="git pipenv npm"
 MEDICAL_DIR="medical-backend"
 MEDICAL_BACKEND_REMOTE="https://github.com/enrique-rodriguez/medical-backend"
+MEDICAL_FRONTEND_REMOTE="https://github.com/enrique-rodriguez/medical-frontend"
 
 
 abort() {
@@ -26,8 +27,16 @@ done
 
 
 mkdir "$MEDICAL_DIR" && cd "$MEDICAL_DIR"
-git clone "$MEDICAL_BACKEND_REMOTE" "$MEDICAL_DIR"
+git clone "$MEDICAL_BACKEND_REMOTE" .
 cd "$MEDICAL_DIR"
 
 pipenv install
 pipenv run setup
+
+cd frontend
+
+git clone "$MEDICAL_FRONTEND_REMOTE" .
+
+npm install
+
+npm run build
