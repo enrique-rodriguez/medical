03e58487f14f0e00cdc29abcc0d413543c74afcf
Enrique Rodriguez <enrique.rodriguez9@upr.edu>
2022-04-26 10:59:29 -0400

    reverse whitenoise config

--- medical/settings.py
+++ medical/settings.py
@@ -38,7 +38,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
@@ -113,8 +112,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / "static"
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [
     BASE_DIR / "medical/static"
