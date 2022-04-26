bcebbece012e2cb9f9ded9db2fd9d769ada0e68f
Enrique Rodriguez <enrique.rodriguez9@upr.edu>
2022-04-26 00:11:13 -0400

    Configure whitenoise

--- medical/wsgi.py
+++ medical/wsgi.py
@@ -1,10 +1,10 @@
 import os
 from django.conf import settings
-# from whitenoise import WhiteNoise
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medical.settings')
 
 application = get_wsgi_application()
 
-# application = WhiteNoise(application, root=settings.STATIC_ROOT)
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
